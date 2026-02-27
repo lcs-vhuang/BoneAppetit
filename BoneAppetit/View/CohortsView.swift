c6f43574836b05e9a1437eb470e2e801cc3331e7
Venessa Huang <vhuang@lcs.on.ca>
2026-02-27 09:41:11 -0500

    Finished cohorts

--- BoneAppetit/View/CohortsView.swift
+++ BoneAppetit/View/CohortsView.swift
@@ -10,11 +10,23 @@ import SwiftUI
 struct CohortsView: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                Image(systemName: "globe")
+            VStack(alignment: .center) {
+                Spacer()
+                Image(systemName: "person.crop.circle.badge.questionmark.fill")
                     .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+                    .font(.system(size: 100, weight: .regular, design: .default))
+                Text("You are not a member of any cohorts.")
+                    .font(.system(size: 35.0, weight: .medium, design: .default))
+                Text("A cohort is a group of people who care for one or more pets.")
+                    .font(.system(size: 20.0, weight: .regular, design: .default))
+                    .padding()
+                Text("Create a new cohort by pressing ➕ or open the app later on to see if you have received an invitation.")
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 20.0, weight: .regular, design: .default))
+                    .padding()
+                    
+                
+                Spacer()
             }
             .padding()
             .navigationTitle("Cohorts")
@@ -23,7 +35,6 @@ struct CohortsView: View {
                 ToolbarItem(placement: .topBarLeading) {
                     
                     Button("Sign out") {
-                        // Does nothing right now
                     }
                     
                 }
@@ -32,8 +43,10 @@ struct CohortsView: View {
                     
                     Button {
                         // Does nothing right now
-                    } label:{Image(systemName: "plus")}
-                    
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .buttonStyle(.borderedProminent)
                 }
                 
             }
