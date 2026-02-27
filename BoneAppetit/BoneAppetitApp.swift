b626d31a1c74bfd5c8adb7df3c12c98ef0e3f4d4
Venessa Huang <vhuang@lcs.on.ca>
2026-02-27 08:41:36 -0500

    Initial Commit

---
+++ BoneAppetit/BoneAppetitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoneAppetitApp.swift
+//  BoneAppetit
+//
+//  Created by 黃翊喬 on 2026/2/27.
+//
+
+import SwiftUI
+
+@main
+struct BoneAppetitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
